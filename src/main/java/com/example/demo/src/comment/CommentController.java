42179dd00a211bb5cead5a5dcc26f26d6fa09a24
msk226 <kminseok5167@gmail.com>
2024-03-12 13:05:31 +0900

    [feature] 댓글 Res, Req 생성 및 API 시그니처 생성

--- src/main/java/com/example/demo/src/comment/CommentController.java
+++ src/main/java/com/example/demo/src/comment/CommentController.java
@@ -1,5 +1,12 @@
 package com.example.demo.src.comment;
 
+import com.example.demo.common.response.BaseResponse;
+import com.example.demo.src.comment.model.GetCommentReq;
+import com.example.demo.src.comment.model.GetCommentRes;
+import com.example.demo.src.comment.model.PatchCommentReq;
+import com.example.demo.src.comment.model.PatchCommentRes;
+import com.example.demo.src.comment.model.PostCommentReq;
+import com.example.demo.src.comment.model.PostCommentRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentController {
 
     // 댓글 작성
-
+    public BaseResponse<PostCommentRes> createComment(PostCommentReq postCommentReq) {
+        return null;
+    }
 
     // 댓글 조회
+    public BaseResponse<GetCommentRes> getComments(GetCommentReq getCommentReq) {
+        return null;
+    }
+
 
     // 댓글 삭제
+    public BaseResponse<String> deleteComment(Long commentId) {
+        return null;
+    }
+
 
     // 댓글 수정
+    public BaseResponse<PatchCommentRes> updateComment(Long commentId, PatchCommentReq patchCommentReq) {
+        return null;
+    }
 
     // 댓글 무한 페이징
 
