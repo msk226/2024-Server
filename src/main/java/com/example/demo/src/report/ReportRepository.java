bf5cafd220d1eafa077ee22bb28c2489994b19d8
msk226 <kminseok5167@gmail.com>
2024-03-13 21:29:19 +0900

    [feature] 구독 신청, 취소 로직 결제 API에 추가

--- src/main/java/com/example/demo/src/report/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.src.report;
-
-import com.example.demo.src.report.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
